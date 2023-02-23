7546b57b6dff5dcec138830dd05cd9305b7f670b
Blessingcharlie <blesscharli30@gmail.com>
2023-02-23 06:54:26 -0800

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int i;
 
 for (i = '0'; i <= '9' ; i++)
 {
- putchar(i);
+putchar(i);
 
- if (i != '9')
- {
-  putchar(',');
-  putchar(' ');
- }
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
 }
 
 putchar('\n');
