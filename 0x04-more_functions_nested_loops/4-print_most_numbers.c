736914926be4b856b996059025ef899f3887c0a0
Blessingcharlie <blesscharli30@gmail.com>
2023-02-23 14:43:27 -0800

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
 * print_most_numbers - checks for checks for a digit (0 through 9).
@@ -10,6 +9,7 @@
 void print_most_numbers(void)
 
 {
+
 int c;
 
 for (c = 48; c < 58; c++)
